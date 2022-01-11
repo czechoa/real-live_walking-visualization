5f2138681f06215abdfd86edc396fea87013f726
czechoa <andrzej.czechu.ja@gmail.com>
2022-01-11 19:34:19 +0100

    add buttons, clean up

--- app_symulation_move.py
+++ app_symulation_move.py
@@ -33,41 +33,36 @@ def plot_single_figure_six_traces_separately_for_all_foots(df_measurements):
     return fig
 
 
-con = sqlite3.connect("proj.db")
+def get_prepared_measurements():
+    con = sqlite3.connect("proj.db")
 
-tmp_last_name = "Grzegorczyk"
+    tmp_last_name = "Grzegorczyk"
 
-df_all_measurement = pd.read_sql_query("SELECT * from measurements", con)  # this moment it is one person
+    df_all_measurement = pd.read_sql_query("SELECT * from measurements", con)  # this moment it is one person
 
-df_all_measurement['value'] = normalised_column_value(df_all_measurement)
+    df_all_measurement['value'] = normalised_column_value(df_all_measurement)
 
-df_all_measurement['time'] = (df_all_measurement['time'] - min(df_all_measurement['time'])) / (
+    df_all_measurement['time'] = (df_all_measurement['time'] - min(df_all_measurement['time'])) / (
             max(df_all_measurement['time']) - min(df_all_measurement['time']))
 
-df_all_measurement = df_all_measurement.drop('index', axis=1)
+    df_all_measurement = df_all_measurement.drop('index', axis=1)
 
-person_measurements = df_all_measurement[df_all_measurement['lastname'] == tmp_last_name]
+    person_measurements = df_all_measurement[df_all_measurement['lastname'] == tmp_last_name]
+    return person_measurements
 
-# current_value = df_all_measurement[df_all_measurement['trace_id'] == max(df_all_measurement['trace_id'])]
-current_value = person_measurements[df_all_measurement['time'] == max(df_all_measurement['time'])]
-# current_value = df_all_measurement.iloc[-6:]
+
+person_measurements = get_prepared_measurements()
 
 min_time = min(person_measurements['time'])
 max_time = max(person_measurements['time'])
 
 img = Image.open('stopki.png')
 
-fig3 = go.Figure(data=[go.Table(header=dict(values=['Time', 'Place']),
-                                cells=dict(values=[[100, 90, 80, 90], [95, 85, 75, 95]]))
-                       ])
-fig3.update_layout(
-    title="Anomaly")
-
 fig_foot = go.Figure(data=[go.Scatter(
     x=[3.5, 1, 3, 6.5, 9, 7],
     y=[7, 6, 1.5, 7, 6, 1.5],
     mode='markers',
-    # marker_size=current_value.iloc[:, -1]
+    # marker_size=person_measurements.iloc[:, -1]
 )],
 )
 
@@ -88,8 +83,8 @@ fig_foot.add_layout_image(
 
 fig_foot.update_layout(
     template="plotly_white",
-    title="current values")
-
+    # title="current values")
+)
 app = dash.Dash(__name__)
 
 app.layout = html.Div(children=[
@@ -100,9 +95,6 @@ app.layout = html.Div(children=[
     html.H1(children='Patterns:'),
     html.H1(children='co to wgl znaczy wzor chodzenia, nwm jakas prenetacja stopek tu pewnie bedzie'),
 
-    html.Button('START', id='button_start'),
-    html.Button('STOP', id='button_stop'),
-
     html.H1(children='Speed'),
     dcc.Slider(
         id='speed-slider',
@@ -112,15 +104,12 @@ app.layout = html.Div(children=[
         value=1,
     ),
 
-
-
     html.H1(children='Time'),
     dcc.Slider(
         id='time-slider',
         min=0,
         max=10,
         step=1,
-        # value=time[-1],
         value=1,
 
     ),
@@ -142,35 +131,70 @@ app.layout = html.Div(children=[
             10: '10',
         }
     ),
-    html.P("Time:"),
-    dcc.RangeSlider(
-        id='range-slider',
-        min=min_time, max=max_time, step=0.1,
-        marks={min_time: str(min_time), max_time: str(max_time)},
-        value=[min_time, max_time],
-        allowCross=False,
-        tooltip={"placement": "bottom", "always_visible": True}
-    ),
 
     dcc.Graph(
         id='scanner_history_foot',
-        # figure=plot_single_figure_six_traces_separately_for_all_foots(df_all_measurement[df_all_measurement['lastname'] == 'Grzegorczyk']),
+        figure=plot_single_figure_six_traces_separately_for_all_foots(person_measurements),
 
-        # style={'width': '180vh', 'height': '180vh'}
     ),
-    dcc.Graph(
-        id='graph_foot',
-        # figure=fig_foot,
-        style={'width': '90vh', 'height': '90vh'}
+    html.Div([
+        dcc.Graph(
+            id='graph_foot',
+            figure=fig_foot,
+            style={'width': '90vh', 'height': '90vh'}
+        ),
+    ],
+        style={'width': '48%', 'float': 'left', 'display': 'inline-block'}
+
     ),
+    html.Div([
+        html.Button('START', id='button_start'),
+        html.Button('STOP', id='button_stop'),
+
+        html.P("Time:"),
+        dcc.RangeSlider(
+            id='range-slider',
+            min=min_time, max=max_time, step=0.1,
+            marks={min_time: str(min_time), max_time: str(max_time)},
+            value=[min_time, (max_time - min_time)/2 , max_time],
+            allowCross=False,
+            tooltip={"placement": "bottom", "always_visible": True},
+
+            pushable= 0.01
+        ),
+    ],
+        style={'width': '48%', 'float': 'right', 'display': 'inline-block'}
+    ),
+    dcc.Interval(
+        id='interval-component',
+        interval=1 * 1000,  # in milliseconds
+        n_intervals=0
+    )
 ])
 
+# @app.callback(Output('output-state', 'children'),
+#               Input('button_start', 'n_clicks'))
+# def update_output(n_clicks):
+#     return u'''
+#         The Button has been pressed {} times,
+#         Input 1 is "{}",
+#         and Input 2 is "{}"
+#     '''.format(n_clicks)
+#
+# @app.callback(Output('output-state', 'children'),
+#               Input('button_stop', 'n_clicks'))
+# def update_output(n_clicks):
+#     return u'''
+#         The Button has been pressed {} times,
+#         Input 1 is "{}",
+#         and Input 2 is "{}"
+#     '''.format(n_clicks)
 
 @app.callback(
     Output("scanner_history_foot", "figure"),
     [Input("range-slider", "value")])
 def update_bar_chart(slider_range):
-    low, high = slider_range
+    low,current, high = slider_range
 
     mask = person_measurements[(person_measurements['time'] > low) & (person_measurements['time'] < high)]
     title = str(person_measurements['firstname'].unique() + ' ' + person_measurements['lastname'].unique())
@@ -202,13 +226,12 @@ def update_bar_chart(slider_range):
 def update_foot_image(hoverData):
     # time = hoverData['points']['x']
     time = hoverData['points'][0]['x']
-    print('time=',time)
 
     fig_foot = go.Figure(data=[go.Scatter(
         x=[3.5, 1, 3, 6.5, 9, 7],
         y=[7, 6, 1.5, 7, 6, 1.5],
         mode='markers',
-        marker_size=person_measurements[person_measurements['time'] == time].iloc[:,-1]
+        marker_size=person_measurements[person_measurements['time'] == time].iloc[:, -1]
     )],
     )
 
@@ -226,11 +249,9 @@ def update_foot_image(hoverData):
             layer="below"),
 
     )
-
     fig_foot.update_layout(
         template="plotly_white",
         title="select values")
-
 
     return fig_foot
 
