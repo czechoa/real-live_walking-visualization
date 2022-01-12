4d4aff31b4ccf700432be203f303c789bec4e8b0
czechoa <andrzej.czechu.ja@gmail.com>
2022-01-12 10:36:20 +0100

    add buttons, clean up

--- app_symulation_move.py
+++ app_symulation_move.py
@@ -62,7 +62,7 @@ fig_foot = go.Figure(data=[go.Scatter(
     x=[3.5, 1, 3, 6.5, 9, 7],
     y=[7, 6, 1.5, 7, 6, 1.5],
     mode='markers',
-    # marker_size=person_measurements.iloc[:, -1]
+    marker_size=person_measurements.iloc[-6:, -1]
 )],
 )
 
@@ -154,7 +154,7 @@ app.layout = html.Div(children=[
         html.P("Time:"),
         dcc.RangeSlider(
             id='range-slider',
-            min=min_time, max=max_time, step=0.1,
+            min=min_time, max=max_time, step=0.01,
             marks={min_time: str(min_time), max_time: str(max_time)},
             value=[min_time, (max_time - min_time)/2 , max_time],
             allowCross=False,
@@ -172,23 +172,6 @@ app.layout = html.Div(children=[
     )
 ])
 
-# @app.callback(Output('output-state', 'children'),
-#               Input('button_start', 'n_clicks'))
-# def update_output(n_clicks):
-#     return u'''
-#         The Button has been pressed {} times,
-#         Input 1 is "{}",
-#         and Input 2 is "{}"
-#     '''.format(n_clicks)
-#
-# @app.callback(Output('output-state', 'children'),
-#               Input('button_stop', 'n_clicks'))
-# def update_output(n_clicks):
-#     return u'''
-#         The Button has been pressed {} times,
-#         Input 1 is "{}",
-#         and Input 2 is "{}"
-#     '''.format(n_clicks)
 
 @app.callback(
     Output("scanner_history_foot", "figure"),
@@ -196,15 +179,41 @@ app.layout = html.Div(children=[
 def update_bar_chart(slider_range):
     low,current, high = slider_range
 
+    delta = 0.01
+
     mask = person_measurements[(person_measurements['time'] > low) & (person_measurements['time'] < high)]
+
+
+
     title = str(person_measurements['firstname'].unique() + ' ' + person_measurements['lastname'].unique())
 
     fig = make_subplots(rows=2, cols=3, start_cell="bottom-left", shared_xaxes=True, shared_yaxes=True,
                         subplot_titles=person_measurements['name'].unique(), x_title='time', y_title='value')
+    print(current)
+
+    point = mask[((mask['time'] > (current - delta)) & (mask['time'] < (current + delta)))][mask['id_sensor'] == 0].iloc[-1]
+
+    print('time ',point['time'])
+
     for i in range(6):
+        point = mask[((mask['time'] > (current - delta)) & (mask['time'] < (current + delta)))][mask['id_sensor'] == i].iloc[-1]
+
         fig.add_trace(go.Scatter(x=mask[mask['id_sensor'] == i]['time'], y=mask[mask['id_sensor'] == i]['value'],
-                                 showlegend=False),
+                                 showlegend=False,mode='lines'),
                       row=int(i / 3) + 1, col=(i % 3) + 1)
+
+        # print(point['time'],point['value'])
+
+
+        fig.add_trace(go.Scatter(x = [point['time']], y= [point['value']],
+                                 showlegend=False,mode= 'markers'),
+                      row=int(i / 3) + 1, col=(i % 3) + 1)
+
+        # fig.add_trace(go.Scatter(x=mask[mask['id_sensor'] == i and  mask['time'] == current]['time'], y=mask[mask['id_sensor'] == i and mask['time'] == current]['value'],
+        #                          showlegend=True,  mode='markers',
+        #             name='markers',marker_size = 50,     fillcolor='red'),
+        #
+        #               row=int(i / 3) + 1, col=(i % 3) + 1)
 
     fig.update_layout(
         title={
@@ -216,9 +225,7 @@ def update_bar_chart(slider_range):
 
     return fig
 
-
 @app.callback(
-    # Output("selected-data", "figure"),
     Output('graph_foot', 'figure'),
     [
         Input('scanner_history_foot', 'hoverData')
@@ -251,10 +258,22 @@ def update_foot_image(hoverData):
     )
     fig_foot.update_layout(
         template="plotly_white",
-        title="select values")
+        # title="select values"
+    )
 
     return fig_foot
 
+@app.callback(
+    Output('range-slider', 'value'),
+    [
+        Input('scanner_history_foot', 'hoverData')
+    ])
+def update_foot_image(hoverData):
+    # time = hoverData['points']['x']
+    time = hoverData['points'][0]['x']
+    axis_min = hoverData[]
+    print(time)
+    return [0,time,1]
 
 if __name__ == '__main__':
     app.run_server(debug=True)
