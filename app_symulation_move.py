77acb25c7ff9edcd82cfeebe0d199575c41f3151
czechoa <andrzej.czechu.ja@gmail.com>
2022-01-13 13:46:39 +0100

    move create start figs and preparing data to new module

--- app_symulation_move.py
+++ app_symulation_move.py
@@ -5,60 +5,18 @@ import pandas as pd
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from PIL import Image
-import sqlite3
 
-# tabelke z anomaliami
-# wartosci srednie (dwa wykresy )
-# powinno byc git
+from prepared_measurements_and_starts_fig import get_prepared_measurements, \
+    plot_single_figure_six_traces_separately_for_all_foots, \
+    create_fig_quartiles, create_fig_foot
 
-def normalised_column_value(df):
-    return df['value'] / max(df['value']) * 100
-
-
-def plot_single_figure_six_traces_separately_for_all_foots(df_measurements):
-    title = str(df_measurements['firstname'].unique() + ' ' + df_measurements['lastname'].unique())
-
-    fig = make_subplots(rows=2, cols=3, start_cell="bottom-left", shared_xaxes=True, shared_yaxes=True,
-                        subplot_titles=df_measurements['name'].unique(), x_title='time', y_title='value')
-
-    for i in range(6):
-        fig.add_trace(go.Scatter(y=df_measurements[df_measurements['id_sensor'] == i]['value'], showlegend=False),
-                      row=int(i / 3) + 1, col=(i % 3) + 1)
-
-    fig.update_layout(
-        title={
-            'text': title[2:-2],
-            'y': 0.9,
-            'x': 0.5,
-            'xanchor': 'center',
-            'yanchor': 'top'})
-
-    return fig
-
-
-def get_prepared_measurements():
-    con = sqlite3.connect("proj.db")
-
-    tmp_last_name = "Grzegorczyk"
-
-    df_all_measurement = pd.read_sql_query("SELECT * from measurements", con)  # this moment it is one person
-
-    df_all_measurement['value'] = normalised_column_value(df_all_measurement)
-
-    df_all_measurement['time'] = (df_all_measurement['time'] - min(df_all_measurement['time'])) / (
-            max(df_all_measurement['time']) - min(df_all_measurement['time']))
-
-    df_all_measurement = df_all_measurement.drop('index', axis=1)
-
-    person_measurements = df_all_measurement[df_all_measurement['lastname'] == tmp_last_name]
-    return person_measurements
-
-
+# global valous (to do move to json, for share data )
 person_measurements = get_prepared_measurements()
 anomaly = person_measurements[person_measurements['anomaly'] == 1]
 min_time = min(person_measurements['time'])
 max_time = max(person_measurements['time'])
 step = 0.001
+delta = 0.01
 slider_left = min_time
 slider_middle = (max_time - min_time) / 2
 slider_right = max_time
@@ -66,38 +24,6 @@ n_intervals = 0
 
 img = Image.open('stopki.png')
 
-fig_foot = go.Figure(data=[go.Scatter(
-    x=[3.5, 1, 3, 6.5, 9, 7],
-    y=[7, 6, 1.5, 7, 6, 1.5],
-    mode='markers',
-    marker_size=person_measurements.iloc[-6:, -1]
-)],
-)
-
-fig_foot.add_layout_image(
-    dict(
-        source=img,
-        xref="x",
-        yref="y",
-        x=0,
-        y=10,
-        sizex=10,
-        sizey=10,
-        sizing="stretch",
-        opacity=0.5,
-        layer="below"),
-
-)
-
-fig_foot.update_layout(
-    template="plotly_white",
-)
-# tmp = {'sentor_'+str(i): person_measurements[person_measurements['name'] == i]['value'].values for i in person_measurements['name'].unique()}
-# box  = pd.DataFrame({'sentor_'+str(i): person_measurements[person_measurements['name'] == i]['value'].values for i in person_measurements['name'].unique()})
-
-fig_quartiles = px.box({'sensor_'+str(i): person_measurements[person_measurements['name'] == i]['value'].values for i in person_measurements['name'].unique()})
-fig_quartiles.update_layout(
-    title="Quartiles")
 app = dash.Dash(__name__)
 
 app.layout = html.Div(children=[
@@ -115,8 +41,6 @@ app.layout = html.Div(children=[
 
         value='Grzegorczyk'
     ),
-    # html.H1(children='co to wgl znaczy wzor chodzenia, nwm jakas prenetacja stopek tu pewnie bedzie'),
-
     dcc.Graph(
         id='scanner_history_foot',
         figure=plot_single_figure_six_traces_separately_for_all_foots(person_measurements),
@@ -125,13 +49,13 @@ app.layout = html.Div(children=[
     html.Div([
         dcc.Graph(
             id='graph_foot',
-            figure=fig_foot,
+            figure=create_fig_foot(person_measurements.iloc[-6:, -1]),
             style={'width': '90vh', 'height': '70vh'}
         ),
         'mean value',
         dcc.Graph(
             id='graph_foot_mean',
-            figure=fig_foot,
+            figure=create_fig_foot(person_measurements.iloc[-6:, -1]),
             style={'width': '90vh', 'height': '70vh'}
         ),
 
@@ -153,7 +77,7 @@ app.layout = html.Div(children=[
         dcc.Dropdown(
             id='speed',
             # options=[{'label': i, 'value': i} for i in df['category'].unique()],
-            options=[{'label': str(i/10), 'value': str(i/10)} for i in range(11)],
+            options=[{'label': str(i / 10), 'value': str(i / 10)} for i in range(11)],
 
             value='0.0'
         ),
@@ -169,7 +93,7 @@ app.layout = html.Div(children=[
 
             pushable=0.01
         ),
-"Anomaly table",
+        "Anomaly table",
         dash_table.DataTable(
             id='datatable-paging-page-count',
             columns=[
@@ -183,7 +107,7 @@ app.layout = html.Div(children=[
         ),
         dcc.Graph(
             id='quartiles',
-            figure=fig_quartiles,
+            figure=create_fig_quartiles(person_measurements),
             style={'width': '90vh', 'height': '60vh'}
         ),
 
@@ -191,7 +115,6 @@ app.layout = html.Div(children=[
         style={'width': '48%', 'float': 'right', 'display': 'inline-block'}
     ),
     #
-
 
     dcc.Interval(
         id='interval-component',
@@ -211,6 +134,7 @@ def update_table(page_current, page_size):
            page_current * page_size:(page_current + 1) * page_size
            ].to_dict('records')
 
+
 @app.callback(
     Output('interval-component', 'disabled'),
     Input('start-stop', 'value')
@@ -223,7 +147,7 @@ def update_output(value):
 
 
 @app.callback(
-    Output("scanner_history_foot", "figure"),
+    [Output("scanner_history_foot", "figure")],
     [Input("range-slider", "value")])
 def update_bar_chart(slider_range):
     low, current, high = slider_range
@@ -244,7 +168,7 @@ def update_bar_chart(slider_range):
     for i in range(6):
         point = \
             mask[((mask['time'] > (current - delta)) & (mask['time'] < (current + delta))) & (
-                        mask['id_sensor'] == i)].iloc[
+                    mask['id_sensor'] == i)].iloc[
                 -1]
 
         fig.add_trace(go.Scatter(x=mask[mask['id_sensor'] == i]['time'], y=mask[mask['id_sensor'] == i]['value'],
@@ -269,6 +193,7 @@ def update_bar_chart(slider_range):
 @app.callback(
     [
         Output('graph_foot', 'figure'),
+        Output('graph_foot_mean', 'figure'),
         Output('range-slider', 'value'),
     ],
     [
@@ -292,7 +217,7 @@ def update_foot_image(hoverData, current_intervals):
             slider_middle = hoverData['points'][0]['x']
         except:
             pass
-
+    # maybe should be here delta time
     time = slider_middle
 
     fig_foot = go.Figure(data=[go.Scatter(
---
+++ prepared_measurements_and_starts_fig.py
@@ -0,0 +1,91 @@
+import sqlite3
+import pandas as pd
+from PIL import Image
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import plotly.express as px
+
+
+def normalised_column_value(df):
+    return df['value'] / max(df['value']) * 100
+
+
+def plot_single_figure_six_traces_separately_for_all_foots(df_measurements):
+    title = str(df_measurements['firstname'].unique() + ' ' + df_measurements['lastname'].unique())
+
+    fig = make_subplots(rows=2, cols=3, start_cell="bottom-left", shared_xaxes=True, shared_yaxes=True,
+                        subplot_titles=df_measurements['name'].unique(), x_title='time', y_title='value')
+
+    for i in range(6):
+        fig.add_trace(go.Scatter(y=df_measurements[df_measurements['id_sensor'] == i]['value'], showlegend=False),
+                      row=int(i / 3) + 1, col=(i % 3) + 1)
+
+    fig.update_layout(
+        title={
+            'text': title[2:-2],
+            'y': 0.9,
+            'x': 0.5,
+            'xanchor': 'center',
+            'yanchor': 'top'})
+
+    return fig
+
+
+def get_prepared_measurements():
+    con = sqlite3.connect("proj.db")
+
+    tmp_last_name = "Grzegorczyk"
+
+    df_all_measurement = pd.read_sql_query("SELECT * from measurements", con)  # this moment it is one person
+
+    df_all_measurement['value'] = normalised_column_value(df_all_measurement)
+
+    df_all_measurement['time'] = (df_all_measurement['time'] - min(df_all_measurement['time'])) / (
+            max(df_all_measurement['time']) - min(df_all_measurement['time']))
+
+    df_all_measurement = df_all_measurement.drop('index', axis=1)
+
+    person_measurements = df_all_measurement[df_all_measurement['lastname'] == tmp_last_name]
+    return person_measurements
+
+
+def create_fig_quartiles(person_measurements: pd.DataFrame):
+    fig_quartiles = px.box(
+        {'sensor_' + str(i): person_measurements[person_measurements['name'] == i]['value'].values for i in
+         person_measurements['name'].unique()})
+    fig_quartiles.update_layout(
+        title="Quartiles")
+
+    return fig_quartiles
+
+
+def create_fig_foot(marker_size):
+    img = Image.open('stopki.png')
+    fig_foot = go.Figure(data=[go.Scatter(
+        x=[3.5, 1, 3, 6.5, 9, 7],
+        y=[7, 6, 1.5, 7, 6, 1.5],
+        mode='markers',
+        marker_size=marker_size
+    )],
+
+    )
+
+    fig_foot.add_layout_image(
+        dict(
+            source=img,
+            xref="x",
+            yref="y",
+            x=0,
+            y=10,
+            sizex=10,
+            sizey=10,
+            sizing="stretch",
+            opacity=0.5,
+            layer="below"),
+
+    )
+
+    fig_foot.update_layout(
+        template="plotly_white",
+    )
+    return fig_foot
