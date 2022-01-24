bedf03f5aebe96cd849a446f9cb77e8ef1676f1c
czechoa <andrzej.czechu.ja@gmail.com>
2022-01-24 15:05:03 +0100

    improve grab_one_serie_for_all_person, now works a litle  faster

--- collect.py
+++ collect.py
@@ -19,9 +19,9 @@ cur.execute("DROP TABLE IF EXISTS measurements")
 sql = ''' INSERT INTO traces(name, L0_val, L1_val, L2_val, R0_val,R1_val,R2_val,time)
               VALUES(?,?,?,?,?,?,?,?) '''
 
-
-
-while True:
+start = time()
+delta = 0
+while delta < 10* 60: # 10 minunts
     dt = requests.get(url)
     dt=dt.json()
     # task=(dt["firstname"]+' '+dt["lastname"],dt["trace"]["sensors"][0]["value"],dt["trace"]["sensors"][1]["value"],dt["trace"]["sensors"][2]["value"]
@@ -33,21 +33,26 @@ while True:
     # measurements = grab_one_serie_for_all_person() # time of that is 0.7 secunds, it too long
 
     # start = time()
-    measurements = grab_one_serie(1) # one person
-    measurements.to_sql('measurements', con=conn, if_exists='append')
-    measurements = grab_one_serie(2)  # one person
-    measurements.to_sql('measurements', con=conn, if_exists='append')
-    measurements = grab_one_serie(3)  # one person
-    measurements.to_sql('measurements', con=conn, if_exists='append')
-    measurements = grab_one_serie(4)  # one person
-    measurements.to_sql('measurements', con=conn, if_exists='append')
-    measurements = grab_one_serie(5)  # one person
-    measurements.to_sql('measurements', con=conn, if_exists='append')
-    measurements = grab_one_serie(6)  # one person
-    measurements.to_sql('measurements', con=conn, if_exists='append')
-    # stop = time()
-    # print(start - stop)    # this procedure take   -0.17148542404174805 for me it to long
+    # measurements = grab_one_serie(1) # one person
+    # measurements.to_sql('measurements', con=conn, if_exists='append')
+    # measurements = grab_one_serie(2)  # one person
+    # measurements.to_sql('measurements', con=conn, if_exists='append')
+    # measurements = grab_one_serie(3)  # one person
+    # measurements.to_sql('measurements', con=conn, if_exists='append')
+    # measurements = grab_one_serie(4)  # one person
+    # measurements.to_sql('measurements', con=conn, if_exists='append')
+    # measurements = grab_one_serie(5)  # one person
+    # measurements.to_sql('measurements', con=conn, if_exists='append')
+    # measurements = grab_one_serie(6)  # one person
+    # measurements.to_sql('measurements', con=conn, if_exists='append')
 
-    # sleep(0.1)
-    # print(task)
+    measurements = grab_one_serie_for_all_person()
+    measurements.to_sql('measurements', con=conn, if_exists='append')
+
+    stop = time()
+    delta = stop - start
+
+    print(delta)    # this procedure take   -0.17148542404174805 for me it to long
+
+    sleep(0.2)
 
--- read_data.py
+++ read_data.py
@@ -6,11 +6,21 @@ import json
 def get_text_measurments_patient( patient_id: int):
     return requests.get('http://tesla.iem.pw.edu.pl:9080/v2/monitor/'+str(patient_id)).json()
 
-def grab_a_serie_of_measurment(id):
-    measurments = []
+def get_text_measurments_patient_for_all_patient( ):
+    return [requests.get('http://tesla.iem.pw.edu.pl:9080/v2/monitor/'+str(patient_id)).json() for patient_id in range(1,7)]
+
+
+def grab_a_serie_of_measurment(id, measurments = []):
     stud_obj = get_text_measurments_patient(id)
     measurments.append(stud_obj)
     return measurments
+
+
+def grab_a_serie_of_measurments_for_all_patients(id, measurments = []):
+    measurments.append(get_text_measurments_patient(id))
+    return measurments
+
+
 
 def convert_measurments_to_df(measurments:list):
     df_measurements_described = pd.DataFrame([{**measurment,**{'trace_id': measurment['trace']['id']}} for measurment in measurments for sensor in measurment['trace']['sensors']])
@@ -28,14 +38,14 @@ def add_current_time():
     return time.time()
 
 def grab_one_serie_for_all_person():
-    df_measurements = pd.DataFrame()
-    for id in range(1,7):
-        measurments = grab_a_serie_of_measurment(id)
-        if id == 1:
-            df_measurements = convert_measurments_to_df(measurments)
+    measurments = []
 
-        else:
-            df_measurements = df_measurements.append(convert_measurments_to_df(measurments), ignore_index=True)
+    for id in range(1,7):
+        measurments = grab_a_serie_of_measurment(id,measurments)
+
+
+    df_measurements = convert_measurments_to_df(measurments)
+
     return df_measurements
 
 def grab_one_serie(id):
@@ -43,14 +53,8 @@ def grab_one_serie(id):
     df_measurements = convert_measurments_to_df(measurments)
     return df_measurements
 # %%
-# import time
-#
-# start = time.time()
-# df_measurement = grab_one_serie_for_all_person()
-# # print(time.time() - start)
-#
-# df_measurement_1 = grab_one_serie_for_all_person()
-# trace_1 = df_measurement['trace_id'].unique()
-# trace_2 = df_measurement_1['trace_id'].unique()
-# serie_1 = df_measurement.iloc[1,-1]
-# serie_2 = df_measurement_1.iloc[1,-1]
+import  time
+start = time.time()
+result = grab_one_serie_for_all_person()
+print(time.time() - start)
+
