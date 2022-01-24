7c37637c79e854aaa2a095fd4bcf68b529f7b934
czechoa <andrzej.czechu.ja@gmail.com>
2022-01-24 20:57:40 +0100

    improve plot_single_figure_six_traces_separately

--- prepared_measurements.py
+++ prepared_measurements.py
@@ -25,12 +25,15 @@ def get_prepared_measurements():
     ]
     choices = [1, 2, 3,4,5,6]
 
-    df_all_measurement['name_val'] =  np.select(conditions, choices)
+    df_all_measurement.insert(1, 'name_val', np.select(conditions, choices))
 
     df_all_measurement['value'] = normalised_column_value(df_all_measurement)
 
-    df_all_measurement['time'] = (df_all_measurement['time'] - min(df_all_measurement['time'])) / (
-            max(df_all_measurement['time']) - min(df_all_measurement['time']))
+    # df_all_measurement['time'] = (df_all_measurement['time'] - min(df_all_measurement['time'])) / (
+    #         max(df_all_measurement['time']) - min(df_all_measurement['time']))
+    df_all_measurement['time'] = (df_all_measurement['time'] - min(df_all_measurement['time'])).astype(int)
+
+
 
     person_measurements = df_all_measurement.drop('index', axis=1)
 
